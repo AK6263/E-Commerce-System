ba29902bc301e652a7582ff108169d96b2c32680
AK6263 <abhaykshirsagar01@gmail.com>
2021-10-21 00:23:19 +0530

    starter

--- main.c
+++ main.c
@@ -1,15 +1,27 @@
 #include<stdio.h>
-void addProduct();
+void addProduct(char*[]);
 void removeProduct(void);
 void search(void);
 
 
 int main(){
-    int option;
+    int option = 1;
     char a[10] = "bruh";
-    printf("Enter the option\n [1] add product\n [2] remove product\n [3] search for product");
-    // scanf("%d", &option);
-    addProduct(*a);
+    printf("Enter the option\n[1] add product\n[2] remove product\n[3] Search for product\n");
+    switch (option) {
+        case 1:
+            addProduct(a);
+            break;
+        case 2:
+            removeProduct();
+            break;
+        case 3:
+            search();
+            break;
+        default:
+            break;
+    }
+    addProduct(a);
 
     return 0;
 }
@@ -17,7 +29,7 @@ int main(){
 
 // Add new products
 void addProduct(char *a[]) {
-    puts(*a);
+    printf("%s\n", a);
 }
 
 // Remove Products from Database
