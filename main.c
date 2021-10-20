371787d6d3a2a68dbad7b237d8c7acf67d61194f
AK6263 <abhaykshirsagar01@gmail.com>
2021-10-21 00:08:44 +0530

    starter files

--- main.c
+++ main.c
@@ -1,4 +1,31 @@
 #include<stdio.h>
-void main(){
-    printf("HEllo world");
+void addProduct();
+void removeProduct(void);
+void search(void);
+
+
+int main(){
+    int option;
+    char a[10] = "bruh";
+    printf("Enter the option\n [1] add product\n [2] remove product\n [3] search for product");
+    // scanf("%d", &option);
+    addProduct(*a);
+
+    return 0;
+}
+
+
+// Add new products
+void addProduct(char *a[]) {
+    puts(*a);
+}
+
+// Remove Products from Database
+void removeProduct() {
+
+}
+
+// Search Function
+void search() {
+
 }
