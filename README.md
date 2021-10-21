6d4730abd71aa16a533ac744ddc702e45259f86b
AK6263 <abhaykshirsagar01@gmail.com>
2021-10-22 01:43:33 +0530

    Product database reading

--- README.md
+++ README.md
@@ -17,3 +17,21 @@ An Online E-commerce System better than Amazon. Amazon ki maa ka ************.
 # Admin Functions
 
 - New_Products(), Remove_Products(), Search()
+
+# Requirements
+
+- Current Stocks of the product 
+- Unique id for each product
+
+# Database Structure
+
+## Product database
+
+The Product database has the following structure new id will be added depending on the last id value
+```
+PRODUCT NAME(string),QUANTITY(int),PRICE(float),ID(int)
+```
+The Following Code Snippet can be used 
+``` c
+fscanf(fptr, "%[^,],%d,%f,%d", product_name, &quantity, &price, &id);
+```
--- main.c
+++ main.c
@@ -2,27 +2,41 @@
 void addProduct(char*[]);
 void removeProduct(void);
 void search(void);
+void readFile(void);
+FILE *fptr;
 
+typedef struct product
+{
+    char product_name[100];
+    int id;
+    int quantity;
+    float price;
+
+}Product;
 
 int main(){
-    int option = 1;
-    char a[10] = "bruh";
-    printf("Enter the option\n[1] add product\n[2] remove product\n[3] Search for product\n");
-    switch (option) {
-        case 1:
-            addProduct(a);
-            break;
-        case 2:
-            removeProduct();
-            break;
-        case 3:
-            search();
-            break;
-        default:
-            break;
-    }
-    addProduct(a);
+    char product_name[100];
+    float price;
+    int id, quantity;
+    Product product;
 
+    fptr = fopen("../database/products.txt","+r");
+
+    if (fptr == NULL)
+    {
+        printf("Bhai lund");
+    }
+    while(fread(&product, sizeof(Product), 1, fptr)) 
+    {
+        printf("Name : %s, Quantity : %d, Price : %f, id : %d",product_name, quantity, price, id);
+        printf("Current position: %ld  \n", ftell(fptr));
+
+    }
+    fscanf(fptr, "%[^,],%d,%f,%d", product_name, &quantity, &price, &id);
+    printf("Name : %s, Quantity : %d, Price : %f, id : %d",product_name, quantity, price, id);
+
+
+    fclose(fptr);
     return 0;
 }
 
@@ -34,6 +48,10 @@ void addProduct(char *a[]) {
 
 // Remove Products from Database
 void removeProduct() {
+
+}
+
+void readFile() {
 
 }
 
